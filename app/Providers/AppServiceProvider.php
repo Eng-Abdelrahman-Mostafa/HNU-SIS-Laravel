31779c2a4e621d5ae1d612fca01559f4ab32d3d2
Eng-Abdelrahman-Mostafa <abdelrahmanmostafa785@gmail.com>
2026-01-19 02:13:08 +0200

    Force HTTPS scheme in production and development environments

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use BezhanSalleh\LanguageSwitch\LanguageSwitch;
 
@@ -20,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if ($this->app->environment('production')||$this->app->environment('development')) {
+            URL::forceScheme('https');
+        }
         LanguageSwitch::configureUsing(function (LanguageSwitch $switch) {
             $switch
                 ->locales(['ar', 'en'])
